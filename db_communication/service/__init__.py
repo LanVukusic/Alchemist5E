cc8cbdb188af9560a377ad1c0ea3eeac9605c9ad
justLeMe <gasper.copi@gmail.com>
2019-02-23 20:46:48 +0100

    syntax

--- db_communication/service/__init__.py
+++ db_communication/service/__init__.py
@@ -1,8 +1,8 @@
 from service import DbConnection
 
 DbConnection.DbConnection.host = "localhost"
-DbConnection.DbConnection.user = "gasper"
-DbConnection.DbConnection.passwd = "GC654cg,,"
+DbConnection.DbConnection.user = "usr"
+DbConnection.DbConnection.passwd = "passwd"
 DbConnection.DbConnection.database = "herbalism"
 
 DbConnection.DbConnection.getConnection()
