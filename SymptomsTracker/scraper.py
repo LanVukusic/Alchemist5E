b87b0d4240b0eac998b5927394efd3e84fd45160
Lan <lan.vukusic@gmail.com>
2019-02-23 21:59:09 +0100

    Multiproccesing implemented for faster stealing speed. Im callling it blackScrapper900

--- SymptomsTracker/scraper.py
+++ SymptomsTracker/scraper.py
@@ -6,7 +6,11 @@ import json
 from tqdm import tqdm
 from time import time
 import random
+import multiprocessing as mp
 
+# number of asynchronous threads for downloading. It is 7 because it is a devider of 28
+# - number of letters of alphabetical order, that we are parsing
+WORKERS_NUM  = 7
 
 jsonHolder = {}
 
@@ -32,12 +36,36 @@ def getSubcategoryList(link, numOfCategories = 5):
 
   return subsymptoms
 
-
-for i in tqdm(mydivs):
-  for j in i.findAll("li", {"class":"bg"}):
-    jsonHolder[j.text] = getSubcategoryList(j.find("a")["href"])
+def startProccess(indexMn, indexMx, workerId):
+    # function that starts scrapping data from downloaded site. Index is the range tha it works on.
+  for i in tqdm(mydivs[indexMn: indexMx]):
+    for j in i.findAll("li", {"class":"bg"}):
+      jsonHolder[j.text] = getSubcategoryList(j.find("a")["href"])
     
+def saveJSON(name="data"):
+  jsn = json.dumps(jsonHolder)
+  with open(name+".json", 'w') as outfile:
+      json.dump(jsn, outfile)
 
-jsn = json.dumps(jsonHolder)
-with open('data.json', 'w') as outfile:
-    json.dump(jsn, outfile)
+
+if __name__ == '__main__':
+  # Setup a list of processes that we want to run
+  lastInedx = 0
+  workers = []
+  for i in range(WORKERS_NUM):  # code breakes if division is not perfect. if WORKERS_NUM does not devide mydivs.len entries will get skipped.
+    minInd = lastInedx
+    maxInd = (len(mydivs) // WORKERS_NUM)+lastInedx
+    workers.append(mp.Process(target=startProccess, args=(minInd, maxInd)))
+    lastInedx = maxInd
+
+    # Run processes
+  for p in workers:
+      print("worker started")
+      p.start()
+
+  # Exit the completed processes
+  for p in workers:
+      print("worker finished")
+      p.join()
+  
+  saveJSON("data")
