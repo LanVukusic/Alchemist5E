b31d0cbf82800bf73d9b98f9bf9d092fc34ab082
justLeMe <gasper.copi@gmail.com>
2019-02-23 23:51:46 +0100

    current state of the alcehmy database

--- herbalism.sql
+++ herbalism.sql
@@ -16,6 +16,52 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `Climite`
+--
+
+DROP TABLE IF EXISTS `Climite`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Climite` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Climite`
+--
+
+LOCK TABLES `Climite` WRITE;
+/*!40000 ALTER TABLE `Climite` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Climite` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ExpDate`
+--
+
+DROP TABLE IF EXISTS `ExpDate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ExpDate` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ExpDate`
+--
+
+LOCK TABLES `ExpDate` WRITE;
+/*!40000 ALTER TABLE `ExpDate` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ExpDate` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `Herbs`
 --
 
@@ -25,17 +71,16 @@ DROP TABLE IF EXISTS `Herbs`;
 CREATE TABLE `Herbs` (
   `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(40) NOT NULL,
-  `seasonId` int(6) unsigned DEFAULT NULL,
   `climateId` int(6) unsigned DEFAULT NULL,
-  `localeId` int(6) unsigned DEFAULT NULL,
   `rarityId` int(6) unsigned DEFAULT NULL,
+  `ingestionId` int(6) unsigned DEFAULT NULL,
   `cost` varchar(30) NOT NULL,
   `effect` text,
   `visual` text,
   `lore` text,
   `world` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +89,6 @@ CREATE TABLE `Herbs` (
 
 LOCK TABLES `Herbs` WRITE;
 /*!40000 ALTER TABLE `Herbs` DISABLE KEYS */;
-INSERT INTO `Herbs` VALUES (2,'Test',0,0,0,0,'Testa',NULL,NULL,NULL,NULL),(3,'testaaa',0,0,0,0,'aaaaa',NULL,NULL,NULL,NULL),(4,'testaaa',0,0,0,0,'aaaaa',NULL,NULL,NULL,NULL),(5,'testaaa',0,0,0,0,'aaaaa',NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `Herbs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,8 +101,8 @@ DROP TABLE IF EXISTS `HerbsPotions`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `HerbsPotions` (
   `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `herbId` int(6) unsigned DEFAULT NULL,
-  `potionId` int(6) unsigned DEFAULT NULL,
+  `herbId` int(6) unsigned NOT NULL,
+  `potionId` int(6) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -71,6 +115,203 @@ LOCK TABLES `HerbsPotions` WRITE;
 /*!40000 ALTER TABLE `HerbsPotions` DISABLE KEYS */;
 /*!40000 ALTER TABLE `HerbsPotions` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `HerbsSeasons`
+--
+
+DROP TABLE IF EXISTS `HerbsSeasons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `HerbsSeasons` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `herbId` int(6) unsigned NOT NULL,
+  `seasonId` int(6) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `HerbsSeasons`
+--
+
+LOCK TABLES `HerbsSeasons` WRITE;
+/*!40000 ALTER TABLE `HerbsSeasons` DISABLE KEYS */;
+/*!40000 ALTER TABLE `HerbsSeasons` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Ingestion`
+--
+
+DROP TABLE IF EXISTS `Ingestion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Ingestion` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Ingestion`
+--
+
+LOCK TABLES `Ingestion` WRITE;
+/*!40000 ALTER TABLE `Ingestion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Ingestion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `PotionType`
+--
+
+DROP TABLE IF EXISTS `PotionType`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PotionType` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PotionType`
+--
+
+LOCK TABLES `PotionType` WRITE;
+/*!40000 ALTER TABLE `PotionType` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PotionType` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Potions`
+--
+
+DROP TABLE IF EXISTS `Potions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Potions` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `cost` int(6) unsigned NOT NULL,
+  `rarityId` int(6) unsigned DEFAULT NULL,
+  `expDateId` int(6) unsigned DEFAULT NULL,
+  `typeId` int(6) unsigned DEFAULT NULL,
+  `ingestionId` int(6) unsigned DEFAULT NULL,
+  `brewing` text,
+  `storing` text,
+  `effect` text,
+  `visual` text,
+  `lore` text,
+  `world` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Potions`
+--
+
+LOCK TABLES `Potions` WRITE;
+/*!40000 ALTER TABLE `Potions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Potions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Rarity`
+--
+
+DROP TABLE IF EXISTS `Rarity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Rarity` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Rarity`
+--
+
+LOCK TABLES `Rarity` WRITE;
+/*!40000 ALTER TABLE `Rarity` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Rarity` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Season`
+--
+
+DROP TABLE IF EXISTS `Season`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Season` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Season`
+--
+
+LOCK TABLES `Season` WRITE;
+/*!40000 ALTER TABLE `Season` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Season` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `SpecSymptom`
+--
+
+DROP TABLE IF EXISTS `SpecSymptom`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `SpecSymptom` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `symptomId` int(6) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `SpecSymptom`
+--
+
+LOCK TABLES `SpecSymptom` WRITE;
+/*!40000 ALTER TABLE `SpecSymptom` DISABLE KEYS */;
+/*!40000 ALTER TABLE `SpecSymptom` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Symptom`
+--
+
+DROP TABLE IF EXISTS `Symptom`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Symptom` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Symptom`
+--
+
+LOCK TABLES `Symptom` WRITE;
+/*!40000 ALTER TABLE `Symptom` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Symptom` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -81,4 +322,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-02-23 21:54:18
+-- Dump completed on 2019-02-23 23:51:09
